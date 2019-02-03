769df97b6e8af32ff7f2ff53ee526a8d9d460f92
Kathleen Doviken <kathleendoviken@gmail.com>
2019-02-02 19:05:43 -0600

    more FAQ

--- src/Components/faq.js
+++ src/Components/faq.js
@@ -18,6 +18,10 @@ class faq extends React.Component {
         return (
           <div>
             <Container>
+              <h1 style={{ marginBottom: "25px" }}>
+                {" "}
+                Frequently Asked Questions{" "}
+              </h1>
               <div className="gray-box faq">OUR CONCEPT</div>
               <p style={{ fontSize: "20px", textAlign: "left" }}>
                 SciCards is a free educational service that utilizes AR
@@ -30,13 +34,51 @@ class faq extends React.Component {
                 will be able to examine bone structures on their cell
                 phone or laptop webcam and test their knowledge.
               </p>
-            <div className="light-box faq" style={{marginBottom: "25px"}}>FUTURE APPLICATIONS</div>
+              <div
+                className="light-box faq"
+                style={{ marginBottom: "25px" }}
+              >
+                FUTURE APPLICATIONS
+              </div>
               <p style={{ fontSize: "20px", textAlign: "left" }}>
                 In the future, SciCards hopes to develop tools to help
                 students learn even more about anatomy. SciCards also
                 hopes to expand into other areas, allowing students to
                 explore science in a more hands on way — without bulky
                 diagrams.
+              </p>
+              <div className="gray-box faq">WHY ANATOMY?</div>
+              <p style={{ fontSize: "20px", textAlign: "left" }}>
+                The United States is projected to experience a shortage
+                of doctors in the near future, and SciCards believes
+                that making more accessible learning study tools will
+                give more students the chance to pursue careers in the
+                medical field. However, SciCards hopes to expand beyond
+                just anatomy though, and move onto cell biology, botany,
+                and other heavily visual science topics.
+              </p>
+              <div
+                className="light-box faq"
+                style={{ marginBottom: "25px" }}
+              >
+                WHAT IS AR?
+              </div>
+              <p style={{ fontSize: "20px", textAlign: "left" }}>
+                AR, or Augmented Reality is when technology is used to
+                overlay digital images over the real world. This allows
+                you to explore visual topics in a more interactive and
+                immersive format.
+              </p>
+              <div className="gray-box faq">
+                HOW CAN I USE SCICARDS?
+              </div>
+              <p style={{ fontSize: "20px", textAlign: "left" }}>
+                SciCards can be used almost anywhere. All you need to
+                use SciCards are two devices, one with a camera. This
+                means that you can take SciCards wherever you want,
+                allowing students to explore without hard to find
+                equipment. We hope that SciCards will be used at home
+                and in classrooms alike.
               </p>
             </Container>
           </div>
